643c6056dbcd742ce67ca4a9c09c85fb3fb2e1c5
awesomequiz <awesomequiz@test.test>
2022-02-14 17:11:27 +0530

    First Commit.

---
+++ MyAwesomeQuiz/app/src/main/java/com/example/myawesomequiz/QuizContract.java
@@ -0,0 +1,19 @@
+package com.example.myawesomequiz;
+
+import android.provider.BaseColumns;
+
+public final class QuizContract {
+
+    private QuizContract() {
+    }
+
+    public static class QuestionsTable implements BaseColumns {
+        public static final String TABLE_NAME = "questions";
+        public static final String COLUMN_QUESTION = "statement";
+        public static final String COLUMN_OPTION1 = "A";
+        public static final String COLUMN_OPTION2 = "B";
+        public static final String COLUMN_OPTION3 = "C";
+        public static final String COLUMN_OPTION4 = "D";
+        public static final String COLUMN_ANSWER = "answer";
+    }
+}
---
+++ MyAwesomeQuiz/app/src/main/java/com/example/myawesomequiz/database/TestAdapter.java
@@ -0,0 +1,83 @@
+package com.example.myawesomequiz.database;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.example.myawesomequiz.Question;
+import com.example.myawesomequiz.QuizContract;
+
+public class TestAdapter {
+
+    protected static final String TAG = "DataAdapter";
+
+    private final Context mContext;
+    private SQLiteDatabase mDb;
+    private DataBaseHelper mDbHelper;
+
+    public TestAdapter(Context context) {
+        this.mContext = context;
+        mDbHelper = new DataBaseHelper(mContext);
+    }
+
+    public TestAdapter createDatabase() throws SQLException {
+        try {
+            mDbHelper.createDataBase();
+        } catch (IOException mIOException) {
+            Log.e(TAG, mIOException.toString() + "  UnableToCreateDatabase");
+            throw new Error("UnableToCreateDatabase");
+        }
+        return this;
+    }
+
+    public TestAdapter open() throws SQLException {
+        try {
+            mDbHelper.openDataBase();
+            mDbHelper.close();
+            mDb = mDbHelper.getReadableDatabase();
+        } catch (SQLException mSQLException) {
+            Log.e(TAG, "open >>"+ mSQLException.toString());
+            throw mSQLException;
+        }
+        return this;
+    }
+
+    public void close() {
+        mDbHelper.close();
+    }
+
+    @SuppressLint("Range")
+    public List<Question> getTestData() {
+        List<Question> questionList = new ArrayList<>();
+
+
+        try {
+            Cursor c = mDb.rawQuery("SELECT * FROM " + QuizContract.QuestionsTable.TABLE_NAME + " limit 10", null);
+
+            if (c.moveToFirst()) {
+                do {
+                    Question question = new Question();
+                    question.setQuestion(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_QUESTION)));
+                    question.setOption1(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION1)));
+                    question.setOption2(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION2)));
+                    question.setOption3(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION3)));
+                    question.setOption4(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION4)));
+                    question.setAnswerNr(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_ANSWER)).trim().toCharArray()[0]);
+                    questionList.add(question);
+                } while (c.moveToNext());
+            }
+
+            c.close();
+            return questionList;
+        } catch (SQLException mSQLException) {
+            Log.e(TAG, "getTestData >>"+ mSQLException.toString());
+            throw mSQLException;
+        }
+    }
+}
